712db699b94b8c4a41ec800d8f13e20f8183e993
Yorick Lardenoit <yorick.lardenoit@hotmail.com>
2018-05-20 16:46:36 +0200

    ingredients and meal class added

---
+++ RestAPI/RestAPI/Model/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace Model
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public int Fat { get; set; }
+        public int Sugars { get; set; }
+
+    }
+}
---
+++ RestAPI/RestAPI/Model/Meal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace Model
+{
+    public class Meal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
