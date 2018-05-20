0d0b913ea468c0a7b07aca4091510da0ee0ee995
Yorick Lardenoit <yorick.lardenoit@hotmail.com>
2018-05-20 16:48:49 +0200

    CookBook class added

---
+++ RestAPI/RestAPI/Model/CookBook.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
+
